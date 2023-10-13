e10d313f71719bb6906223d9bdb85cf268ce782b
nishino <nishino@asojuku.ac.jp>
2023-10-13 19:34:34 +0900

    initial commit

---
+++ src/test/java/com/classroom/assignment/service/ArchievementServiceTest.java
@@ -0,0 +1,20 @@
+package com.classroom.assignment.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ArchievementServiceTest {
+
+  @Autowired
+  private ArchievementService service;
+
+  @Test
+  public void test() throws Exception {
+    // given
+    // when
+    // then
+  }
+}
